e057b7f96ac86b42f33c70403c9ef295015dd83c
Gu <jgu@MAC20085.local>
2017-07-07 11:47:26 -0700

    mini line charts working

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,10 @@ console.log("HELLO");
 
 
 // lists of things to loop through
-var data_ids = ['#winning', '#runs', '#batting', '#homeruns', '#ops', '#era'];
+var data_ids = ['#chart0', '#chart1', '#chart2', '#chart3', '#chart4', '#chart5'];
 var all_data = [winningData, runsData, battingData, homerunsData, opsData, eraData];
 var y_ranges = [[.3, .65], [3.6, 4.8], [.24, .27], [.7, .9], [.65, .75], [3.5, 4.9]];
-var y_axis_labels = ["Winning percentage (wins per game)", "Runs per game", "Batting average", "Home runs per game", "OPS (on-base plus slugging)", "ERA (earned run average)"]
-// var winning_focus, runs_focus, batting_focus, homeruns_focus, ops_focus, era_focus;
-// var focuses = [winning_focus, runs_focus, batting_focus, homeruns_focus, ops_focus, era_focus];
+var y_axis_labels = ["Winning percentage (wins per game)", "Runs per game", "Batting average", "Home runs per game", "OPS (on-base plus slugging)", "ERA (earned run average)"];
 
 var margin = {top: 20, right: 30, bottom: 40, left: 55},
 	width = 350 - margin.right - margin.left,
@@ -20,15 +18,12 @@ var margin = {top: 20, right: 30, bottom: 40, left: 55},
 // labels for x axis
 var seasons = ["First half 2016", "Second half 2016", "First half 2017"];
 
-var x = d3.scaleLinear().range([0, width]),
-	y = d3.scaleLinear().range([height, 0]);
-
 var i = 0;
 
 // do everything in here
-//for (var i = 0; i < all_data.length; i++) {
-	// var x = d3.scaleLinear().range([0, width]),
-	// 	y = d3.scaleLinear().range([height, 0]);
+for (var i = 0; i < all_data.length; i++) {
+	var x = d3.scaleLinear().range([0, width]),
+		y = d3.scaleLinear().range([height, 0]);
 
 	// define axes
 	var xAxis = d3.axisBottom(x);
@@ -36,16 +31,11 @@ var i = 0;
 
 	var bisectDate = d3.bisector(function(d) { return d.time; }).right;
 
-
 	var data = all_data[i];
 
 	var line = d3.line()
 		.x(function(d) { return x(d.time); })
-		.y(function(d) { 
-			//console.log(y(d.val));
-			return y(d.val); });
-
-	//console.log(x(.633));
+		.y(function(d) { return y(d.val); });
 
 	var svg = d3.select(data_ids[i])
 		.append("svg")
@@ -54,20 +44,17 @@ var i = 0;
 		.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // set range for each chart
 	var range = y_ranges[i];
-
-//d3.csv(data, function(data) {
-
 	x.domain([-.3, 2.3]);
 	y.domain(range);
-
-	//console.log(y.domain);
 
 	svg.append("path")
 		.data([data])
 		.attr("class", "line")
 		.attr("d", line);
 
+    // draw x axis
 	svg.append("g")
 		.attr("transform", "translate(0," + height + ")")
 		.call(d3.axisBottom(x).ticks(2)
@@ -76,7 +63,7 @@ var i = 0;
 				return seasons[d];
 		}));
 
-	  // text label for the x axis
+	// text label for the x axis
   	svg.append("text") 
   	.attr("class", "axis-label")            
       .attr("transform",
@@ -85,6 +72,7 @@ var i = 0;
       .style("text-anchor", "middle")
       .text("Season");
 
+    // draw y axis
 	svg.append("g")
 		.call(d3.axisLeft(y).ticks(3));
 
@@ -98,8 +86,6 @@ var i = 0;
       	.style("text-anchor", "middle")
       	.text(y_axis_labels[i]);   
 
-	// var thisFocus = focuses[i];
-
 	var thisFocus = svg.append("g")
       .attr("class", "focus")
       .style("display", "none");
@@ -111,20 +97,15 @@ var i = 0;
     	.attr("x", 9)
     	.attr("dy", ".35em");
 
-    // console.log(all_data[i]);
-
-    	//console.log(data);
-
     svg.selectAll("rect")
     	.data(all_data[i])
     	.enter()
     	.append("rect")
-    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    	.attr("transform", "translate(" + margin.left-20 + "," + margin.top + ")")
     	.attr("class", "overlay")
     	.attr("width", width)
     	.attr("height", height)
     	.on("mouseover", function(d) { 
-    		//console.log(d);
     	    var thisFocus = this.parentNode.getElementsByClassName("focus")[0];
     		var thisFocus = d3.select(thisFocus);
     		thisFocus.style("display", "block"); 
@@ -134,26 +115,24 @@ var i = 0;
     		var thisFocus = d3.select(thisFocus);
     		thisFocus.style("display", "none"); })
     	.on("mousemove", function(d) {
-    		// console.log(d);
+            // update y-range
+            var currID = this.parentNode.parentNode.parentNode.getAttribute('id');
+    		var i = currID[5];
+            var range = y_ranges[i];
+            y.domain(range);
+
+            // get correct data for current mouse location
     		var fullData = d3.selectAll("rect").data();
     		var indexOfLast = d3.selectAll("rect").data().indexOf(d);
     		var currData = [];
     		currData.push(fullData[indexOfLast-2]);
     		currData.push(fullData[indexOfLast-1]);
     		currData.push(fullData[indexOfLast]);
-    		//console.log(currData);
     		
     		var xPos = x.invert(d3.mouse(this)[0]) + .7;
-    		// console.log(xPos);
     		var _index = bisectDate(currData, xPos) - 1;
-    		var _d = currData[_index];
-
-    		//console.log(_d);
-    		//console.log(_d.val);
-    		console.log(y(_d.val));
-    		//console.log(x(_d.time));
-    		
-
+    		var _d = currData[_index]; 		
+    	
     		var thisFocus = this.parentNode.getElementsByClassName("focus")[0];
     		var thisFocus = d3.select(thisFocus);
     		thisFocus.attr("transform", "translate(" + x(_d.time) + "," + y(_d.val) + ")");
@@ -161,34 +140,6 @@ var i = 0;
 
     		    	
     	});
-    
 
-    // function mousemove() {
-    // 	//console.log(data);
-    // 	console.log(all_data[i]);
-    	
-    // 	var xPos = x.invert(d3.mouse(this)[0]) + .7;
-    // 	//console.log(xPos);
-    // 	//console.log(this);
-    	
-    // 	var _i = bisectDate(data, xPos) - 1;
-    // 	var d = data[_i];
-
-    // 	//console.log(d.val);
-
-    // 	var thisFocus = this.parentNode.getElementsByClassName("focus")[0];
-    // 	var thisFocus = d3.select(thisFocus);
-    // 	// console.log(thisFocus);
-    	
-    // 	thisFocus.attr("transform", "translate(" + x(d.time) + "," + y(d.val) + ")");
-    // 	thisFocus.select("text").text(d.val);
-
-    // 	// thisFocus.attr("translate", function(d) {
-    // 	// 	console.log(d);
-
-    // 	// })
-    // }
-//})
-
-//}
+}
 
